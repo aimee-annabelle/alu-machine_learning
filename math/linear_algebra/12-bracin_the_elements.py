bb090785d9b0686f9c41891a7b493a270dd708aa
Annabelle Ineza <54577405+aimee-annabelle@users.noreply.github.com>
2025-01-30 18:35:52 +0000

    removed loops and conditionals

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,38 +1,10 @@
 #!/usr/bin/env python3
-"""
-Module for performing element-wise operations on arrays
-"""
+"""Performs element-wise operations on matrices"""
 
 
 def np_elementwise(mat1, mat2):
-    """
-    Performs element-wise addition, subtraction, multiplication, and division.
-    
-    Parameters:
-        mat1: First input array
-        mat2: Second input array or scalar
-    
-    Returns:
-        tuple: Contains (sum, difference, product, quotient) of the element-wise operations
-    """
-    # If mat2 is a scalar, convert it to a matrix of the same shape as mat1
-    if isinstance(mat2, (int, float)):
-        mat2 = [[mat2 for _ in range(len(mat1[0]))] for _ in range(len(mat1))]
-    
-    # Element-wise addition
-    addition = [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))] 
-                for i in range(len(mat1))]
-    
-    # Element-wise subtraction
-    subtraction = [[mat1[i][j] - mat2[i][j] for j in range(len(mat1[0]))] 
-                   for i in range(len(mat1))]
-    
-    # Element-wise multiplication
-    multiplication = [[mat1[i][j] * mat2[i][j] for j in range(len(mat1[0]))] 
-                      for i in range(len(mat1))]
-    
-    # Element-wise division
-    division = [[mat1[i][j] / mat2[i][j] for j in range(len(mat1[0]))] 
-                for i in range(len(mat1))]
-    
-    return (addition, subtraction, multiplication, division)
+    """Return tuple of element-wise operations without loops or conditionals"""
+    return (mat1 + mat2,
+            mat1 - mat2,
+            mat1 * mat2,
+            mat1 / mat2)
