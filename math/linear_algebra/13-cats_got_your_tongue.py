313623c756c55fc67c5ef9549b2dc0c7d26baaf9
Annabelle Ineza <54577405+aimee-annabelle@users.noreply.github.com>
2025-01-30 18:40:11 +0000

    petty spaces😏

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -8,12 +8,12 @@ import numpy as np
 def np_cat(mat1, mat2, axis=0):
     """
     Concatenates two matrices along a specific axis
-    
+
     Parameters:
         mat1 (numpy.ndarray): First input matrix
         mat2 (numpy.ndarray): Second input matrix
         axis (int): Axis along which to concatenate (default=0)
-    
+
     Returns:
         numpy.ndarray: Concatenated matrix
     """
